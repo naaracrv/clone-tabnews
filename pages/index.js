75565abfcc3e908ec3e081f374c892dae6818463
Naara Veronez <naaracristinarv@gmail.com>
2023-12-04 22:27:07 +0000

    adicionando a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return( 
+        <div style={{textAlign:"center", margin:50}}>
+            <h1>Te amo e to com sdds </h1>
+            <h2>Se você viu essa mensagem, me mande uma figurinha ou um emoji de coração</h2>
+        </div>
+        )
+}
